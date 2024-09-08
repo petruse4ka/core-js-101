ef9a78508574698271313a4f70d66b1fd4156c61
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2024-09-09 00:27:48 +0200

    feat: add solution to 6 more tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -90,8 +90,9 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  const result = (0 - b) / a;
+  return result;
 }
 
 
@@ -113,9 +114,14 @@ function getLinearEquationRoot(/* a, b */) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const top = x1 * x2 + y1 * y2;
+  const firstBottom = x1 * x1 + y1 * y1;
+  const secondBottom = x2 * x2 + y2 * y2;
+  const result = Math.acos(top / (Math.sqrt(firstBottom) * Math.sqrt(secondBottom)));
+  return result;
 }
+
 
 /**
  * Returns a last digit of a integer number.
@@ -129,8 +135,9 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const result = Number(String(value).slice(-1));
+  return result;
 }
 
 
@@ -145,8 +152,9 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  const result = Number(value);
+  return result;
 }
 
 /**
@@ -162,8 +170,9 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  const result = Math.sqrt(a * a + b * b + c * c);
+  return result;
 }
 
 
@@ -184,8 +193,10 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const roundValue = 10 ** pow;
+  const result = Math.round(num / roundValue) * roundValue;
+  return result;
 }
 
 /**
